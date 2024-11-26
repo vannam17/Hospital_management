23a2e34392c6f948c77952b49cb47f824aba6c5a
vannam17 <nam098717265@gmail.com>
2024-11-26 22:31:10 +0700

    update

---
+++ src/main/java/com/db/DBConnect.java
@@ -0,0 +1,22 @@
+package com.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+
+	private static Connection conn;
+	
+	public static Connection getConn() 
+	{
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_management", "root", "0984092365Nv");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+		
+	}
+}
---
+++ src/main/java/com/user/servlet/UserRegister.java
@@ -0,0 +1,53 @@
+package com.user.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import com.dao.UserDao;
+import com.db.DBConnect;
+import com.entity.User;
+
+
+
+
+
+@WebServlet("/user_register")
+public class UserRegister extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		try {
+			String fullName = req.getParameter("fullname");
+			String email = req.getParameter("email");
+			String password = req.getParameter("password");
+
+			User u = new User(fullName, email, password);
+
+			UserDao dao = new UserDao(DBConnect.getConn());
+
+			HttpSession session = req.getSession();
+
+			boolean f = dao.register(u);
+
+			if (f) {
+
+				session.setAttribute("sucMsg", "Register Sucessfully");
+				resp.sendRedirect("signup.jsp");
+
+			} else {
+				session.setAttribute("errorMsg", "Something wrong on server");
+				resp.sendRedirect("signup.jsp");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
